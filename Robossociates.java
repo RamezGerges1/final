88bfe4c2023719f2bd6bd28e277ebd0c28c527b4
RamezGerges1 <131922676+RamezGerges1@users.noreply.github.com>
2024-04-17 16:06:46 -0400

    Update Robossociates.java

--- Robossociates.java
+++ Robossociates.java
@@ -1,6 +1,6 @@
 public class Robossociates extends TRM{
 	public void participateInDeal() {
-        System.out.println("Robossociates are participating in the deal.");
+        System.out.println("Robossociates are assisting "+ NewMember+ " in the deal.");
     }
 
 }
