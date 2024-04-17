3a55eae25b75e21431bb8650b45a6c3d15214875
RamezGerges1 <131922676+RamezGerges1@users.noreply.github.com>
2024-04-17 16:32:57 -0400

    Update Robossociates.java

--- Robossociates.java
+++ Robossociates.java
@@ -1,6 +1,16 @@
 public class Robossociates extends TRM{
-	public void participateInDeal() {
+	static int money;
+	public static void participateInDeal() {
         System.out.println("Robossociates are assisting "+ NewMember+ " in the deal.");
     }
-
+public static void sellProduct(){
+	Customers.purchaseProduct()
+	if (!Customer.butProduct){
+		System.out.println("I didn't want to sell to you any ways");
+	}
+	else{
+		Customers.ProductAmmount();
+		money= Customer.amount * 10;
+		Customers.paymentMethod();		
+	}
 }
